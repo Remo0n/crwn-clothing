13354233a5fefdcebfbb65647463248a20b4b83c
Remoon <remoon.peter@gmail.com>
2020-12-03 02:27:18 +0200

    Initialize the routes and firebase auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
+import Navbar from "./components/navbar/navbar";
+import SigninAndSignup from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-function App() {
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  let unsubscribeFromAuth = null;
+  useEffect(() => {
+    unsubscribeFromAuth = auth.onAuthStateChanged(async userAuthInfo => {
+      if (userAuthInfo) {
+        const userRef = await createUserProfileDocument(userAuthInfo);
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({ id: snapShot.id, ...snapShot.data() });
+        });
+      } else {
+        setCurrentUser(null);
+      }
+    });
+    return () => {
+      unsubscribeFromAuth();
+    };
+  }, [unsubscribeFromAuth]);
+
+  console.log(currentUser);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar currentUser={currentUser} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SigninAndSignup} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
