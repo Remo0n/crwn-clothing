834e884bb6d9ce335fd4a135570506136a9734a1
Remoon <remoon.peter@gmail.com>
2020-12-05 18:49:06 +0200

    Install redux-persist and setup its config to save the cart reducer in the local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userRducer from "./user/user.reducer";
 import cartRducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ["cart"]
+};
+
+const rootReducer = combineReducers({
   user: userRducer,
   cart: cartRducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
